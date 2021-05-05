caee69249a3e96c6929dbbff04e8d5018d79103d
Raqbit <raqbit@outlook.com>
2021-05-05 14:56:30 +0200

    Add fabric base

---
+++ README.md
@@ -0,0 +1,5 @@
+# DyeingCreepers mod
+
+![Gradle build](https://github.com/Raqbit/DyeingCreepers/workflows/Gradle%20build/badge.svg)
+
+Allows you to dye creepers like sheep.
---
+++ src/main/java/it/raqb/dyeingcreepers/fabric/DyeingCreepersMod.java
@@ -0,0 +1,10 @@
+package it.raqb.dyeingcreepers.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DyeingCreepersMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
+}
